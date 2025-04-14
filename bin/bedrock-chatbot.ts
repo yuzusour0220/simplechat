4dd52ae51f5af33add500f5c030e169a22a7be93
K Sasaki <109719236+keisskaws@users.noreply.github.com>
2025-04-15 01:19:30 +0900

    Update bedrock-chatbot.ts

--- bin/bedrock-chatbot.ts
+++ bin/bedrock-chatbot.ts
@@ -6,7 +6,8 @@ import { BedrockChatbotStack } from '../lib/bedrock-chatbot-stack';
 const app = new cdk.App();
 new BedrockChatbotStack(app, 'BedrockChatbotStack', {
   // モデルIDをオプションで指定可能
-  // modelId: 'us.amazon.nova-lite-v1:0',
+  modelId: 'us.amazon.nova-lite-v1:0',
+  //modelId: 'us.amazon.nova-micro-v1:0',
   
   // 環境変数から取得したリージョンを使用、またはデフォルトとしてus-east-1を使用
   env: { 
